638e37302ad95e7f8c050bd2fc129167b7c6a5c2
santhosh2401 <santhoshgovind007@gmail.com>
2020-08-10 19:24:46 +0530

    Basic Backend

---
+++ ContactManager/app/src/main/java/com/androidunleashed/contactmanager/model/Contacts.java
@@ -0,0 +1,42 @@
+package com.androidunleashed.contactmanager.model;
+
+public class Contacts {
+
+    private int id;
+    private String name;
+    private String phoneNumber;
+
+    public Contacts() {
+
+    }
+
+    public Contacts(int id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ ContactManager/app/src/main/java/com/androidunleashed/contactmanager/util/Util.java
@@ -0,0 +1,16 @@
+package com.androidunleashed.contactmanager.util;
+
+public class Util {
+
+    //DATABASE RELATED
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "contact_db";
+    public static final String TABLE_NAME = "contacts";
+
+    //TABLE RELATED (COLUMNS)
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONENO = "phone_number";
+    
+}
