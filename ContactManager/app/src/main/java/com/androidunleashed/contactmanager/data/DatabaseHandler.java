e7136d9f75cf1cdc24c91a5270746673ffcf2355
santhosh2401 <santhoshgovind007@gmail.com>
2020-08-10 19:53:16 +0530

    Setting up DatabaseHandler Class

--- ContactManager/app/src/main/java/com/androidunleashed/contactmanager/data/DatabaseHandler.java
+++ ContactManager/app/src/main/java/com/androidunleashed/contactmanager/data/DatabaseHandler.java
@@ -6,19 +6,38 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.androidunleashed.contactmanager.R;
+import com.androidunleashed.contactmanager.util.Util;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context,String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION );
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        // to create a table
+        // CREATE TABLE CONTACTS (ID INT,NAME TEXT,PHONENO TEXT);
+        
+        // ; HERE
+        
+        String CREATE_TABLE = "CREATE TABLE "+Util.DATABASE_NAME+" ( "+ Util.KEY_ID+" INTEGER PRIMARY KEY, "+Util.KEY_NAME+" TEXT, "+Util.KEY_PHONENO +" TEXT ); " ;
+        db.execSQL(CREATE_TABLE);
+        
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        //droping table
+
+        String DROP_TABLE = String.valueOf(R.string.drop_table);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+
+        onCreate(db);
 
     }
 }
--- ContactManager/app/src/main/java/com/androidunleashed/contactmanager/util/Util.java
+++ ContactManager/app/src/main/java/com/androidunleashed/contactmanager/util/Util.java
@@ -12,5 +12,5 @@ public class Util {
     public static final String KEY_ID = "id";
     public static final String KEY_NAME = "name";
     public static final String KEY_PHONENO = "phone_number";
-    
+
 }
