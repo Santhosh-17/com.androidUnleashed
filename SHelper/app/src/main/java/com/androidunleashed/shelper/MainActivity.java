2826268b5cc1bfec869c074496e08ce29536add4
santhosh2401 <santhoshgovind007@gmail.com>
2020-10-23 23:56:17 +0530

    Finished PopUp Screen

---
+++ SHelper/app/src/main/java/com/androidunleashed/shelper/MainActivity.java
@@ -0,0 +1,85 @@
+package com.androidunleashed.shelper;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+    private Button SaveButton;
+    private EditText ItemName,Quantity,Colour,Size;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopUpDialogue();
+
+            }
+        });
+    }
+
+    private void createPopUpDialogue() {
+
+        builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup,null);
+
+        ItemName = view.findViewById(R.id.babyItem);
+        Quantity = view.findViewById(R.id.itemQuantity);
+        Colour = view.findViewById(R.id.ItemColor);
+        Size = view.findViewById(R.id.ItemSize);
+        SaveButton = findViewById(R.id.saveButton);
+
+        builder.setView(view);
+        dialog = builder.create(); //creating dialogue object
+        dialog.show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
