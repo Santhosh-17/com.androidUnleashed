a0854018367abcde4d1227ed75443dea10d2521c
santhosh2401 <santhoshgovind007@gmail.com>
2020-11-30 17:59:36 +0530

    Initial Commit

---
+++ NoDo/app/src/main/java/com/androidunleashed/nodo/model/NoDo.java
@@ -0,0 +1,37 @@
+package com.androidunleashed.nodo.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "nodo_table")
+public class NoDo {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "nodo_col")
+    private String noDo;
+
+    public NoDo(@NonNull String noDo) {
+        this.noDo = noDo;
+    }
+
+    public String getNoDo() {
+        return noDo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNoDo(@NonNull String noDo) {
+        this.noDo = noDo;
+    }
+}
